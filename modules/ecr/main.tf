23e00d9bf5e872ed7d2c9b264cc045d852edcb20
John Ritsema <john_ritsema@yahoo.com>
2022-06-08 17:34:16 -0400

    pre-commit run --all-files

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -8,7 +8,7 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = var.name
   image_tag_mutability = var.image_tag_mutability
-  tags = var.tags
+  tags                 = var.tags
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
