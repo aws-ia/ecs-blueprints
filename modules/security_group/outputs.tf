f468d4d26467efe5f3cbce554fcf74e3f8dd33cf
Bryant Biggs <bryantbiggs@gmail.com>
2022-06-27 18:59:01 -0400

    refactor: Replace local ALB and security group modules with external modules

--- modules/security_group/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-output "sg_id" {
-  description = "The ID of the security group"
-  value       = aws_security_group.sg.id
-}
