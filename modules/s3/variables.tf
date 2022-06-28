0c65d02521eb67c2ed6ce3d06b49ed2376e21d9a
Bryant Biggs <bryantbiggs@gmail.com>
2022-06-28 06:53:06 -0400

    refactor: Replace local S3 module with external S3 module (#14)

--- modules/s3/variables.tf
+++
@@ -1,7 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-variable "bucket_name" {
-  description = "The name of your S3 bucket"
-  type        = string
-}
