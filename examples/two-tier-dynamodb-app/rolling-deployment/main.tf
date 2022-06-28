4ad55e793c26d23c29f62997db1cb34d3ed95452
Bryant Biggs <bryantbiggs@gmail.com>
2022-06-28 06:54:14 -0400

    refactor: Replace local DynamoDB module with external DynamoDB module (#16)

--- examples/two-tier-dynamodb-app/rolling-deployment/main.tf
+++ examples/two-tier-dynamodb-app/rolling-deployment/main.tf
@@ -114,7 +114,7 @@ module "ecs_role" {
 
   name               = var.iam_role_name["ecs"]
   name_ecs_task_role = var.iam_role_name["ecs_task_role"]
-  dynamodb_table     = [module.dynamodb_table.dynamodb_table_arn]
+  dynamodb_table     = [module.assets_dynamodb_table.dynamodb_table_arn]
 }
 
 # ------- Creating a IAM Policy for role -------
@@ -353,7 +353,7 @@ module "codebuild_server" {
   service_port           = var.port_app_server
   ecs_role               = var.iam_role_name["ecs"]
   ecs_task_role          = var.iam_role_name["ecs_task_role"]
-  dynamodb_table_name    = module.dynamodb_table.dynamodb_table_name
+  dynamodb_table_name    = module.assets_dynamodb_table.dynamodb_table_id
 }
 
 # ------- Creating the client CodeBuild project -------
@@ -425,10 +425,13 @@ module "assets_s3_bucket" {
 }
 
 # ------- Creating Dynamodb table by the Back-end -------
-module "dynamodb_table" {
-  source = "./../../../modules/dynamodb"
+module "assets_dynamodb_table" {
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "~> 2.0"
 
-  name = "assets-table-${local.name}"
+  name = "${local.name}-assets"
+
+  tags = local.tags
 }
 
 ################################################################################
