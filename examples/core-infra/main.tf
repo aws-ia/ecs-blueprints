22aee491d83d2a3e5d56a1f3f8e4f21f7903dac1
Arvind Soni <arvindsoni@gmail.com>
2022-07-26 09:14:21 -0700

    formating fix

--- examples/core-infra/main.tf
+++ examples/core-infra/main.tf
@@ -8,9 +8,9 @@ locals {
   name   = var.core_stack_name
   region = var.aws_region
 
-  vpc_cidr = var.vpc_cidr
-  num_of_subnets = min(length(data.aws_availability_zones.available.names),3)
-  azs      = slice(data.aws_availability_zones.available.names, 0, local.num_of_subnets)
+  vpc_cidr       = var.vpc_cidr
+  num_of_subnets = min(length(data.aws_availability_zones.available.names), 3)
+  azs            = slice(data.aws_availability_zones.available.names, 0, local.num_of_subnets)
 
   tags = {
     Blueprint  = local.name
