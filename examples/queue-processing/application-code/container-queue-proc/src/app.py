5d5f591f4482ce448bdc43cacc1daa6886746389
Nathan Ness <nathan79@gmail.com>
2022-08-16 20:04:18 -0700

    fixed path

--- examples/queue-processing/application-code/container-queue-proc/src/app.py
+++ examples/queue-processing/application-code/container-queue-proc/src/app.py
@@ -124,8 +124,8 @@ def usage_demo():
             bucket = body['Records'][0]['s3']['bucket']['name']
             key = body['Records'][0]['s3']['object']['key']
             file_name = os.path.split(key)
-            download_path = './tmp/' + file_name[1]
-            upload_path = './tmp/thumbnail-{}'.format(file_name[1])
+            download_path = '/tmp/' + file_name[1]
+            upload_path = '/tmp/thumbnail-{}'.format(file_name[1])
             s3_client.download_file(bucket, key, download_path)
             resize_image(download_path, upload_path)
             s3_client.upload_file(upload_path, DEST_BUCKET, key)
