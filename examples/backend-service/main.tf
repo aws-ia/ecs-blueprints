2e35af28af550262041a01395e9c152791045144
Nathan Ness <nathan79@gmail.com>
2023-03-11 09:30:35 -0700

    removed ci/cd from lb-service and backend-service

--- examples/backend-service/main.tf
+++ examples/backend-service/main.tf
@@ -21,19 +21,6 @@ locals {
 # ECS Blueprint
 ################################################################################
 
-module "container_image_ecr" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "~> 1.4"
-
-  repository_name                   = local.container_name
-  repository_force_delete           = true
-  create_lifecycle_policy           = false
-  repository_read_access_arns       = [one(data.aws_iam_roles.ecs_core_infra_exec_role.arns)]
-  repository_read_write_access_arns = [module.codepipeline_ci_cd.codepipeline_role_arn]
-
-  tags = local.tags
-}
-
 resource "aws_service_discovery_service" "this" {
   name = local.name
 
@@ -82,7 +69,7 @@ module "ecs_service_definition" {
   }
 
   service_connect_configuration = {
-    enabled = true
+    enabled = false
     service = {
       client_alias = [{
         port     = local.container_port
@@ -101,7 +88,7 @@ module "ecs_service_definition" {
   container_definitions = {
     main_container = {
       name  = local.container_name
-      image = module.container_image_ecr.repository_url
+      image = "public.ecr.aws/aws-containers/ecsdemo-nodejs:c3e96da"
 
       port_mappings = [{
         name : "${local.container_name}-${local.container_port}"
@@ -113,154 +100,6 @@ module "ecs_service_definition" {
   }
 
   ignore_task_definition_changes = false
-
-  tags = local.tags
-}
-
-################################################################################
-# CodePipeline and CodeBuild for CI/CD
-################################################################################
-
-module "codepipeline_s3_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 3.0"
-
-  bucket = "codepipeline-${local.region}-${random_id.this.hex}"
-  acl    = "private"
-
-  # For example only - please re-evaluate for your environment
-  force_destroy = true
-
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = local.tags
-}
-
-resource "aws_sns_topic" "codestar_notification" {
-  name = local.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid      = "WriteAccess"
-        Effect   = "Allow"
-        Action   = "sns:Publish"
-        Resource = "arn:aws:sns:${local.region}:${data.aws_caller_identity.current.account_id}:${local.name}"
-        Principal = {
-          Service = "codestar-notifications.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = local.tags
-}
-
-module "codebuild_ci" {
-  source = "../../modules/codebuild"
-
-  name           = "codebuild-${module.ecs_service_definition.name}"
-  service_role   = module.codebuild_ci.codebuild_role_arn
-  buildspec_path = "./application-code/ecsdemo-nodejs/templates/buildspec.yml"
-  s3_bucket      = module.codepipeline_s3_bucket
-
-  environment = {
-    privileged_mode = true
-    environment_variables = [
-      {
-        name  = "REPO_URL"
-        value = module.container_image_ecr.repository_url
-        }, {
-        name  = "CONTAINER_NAME"
-        value = local.container_name
-        }, {
-        name  = "FOLDER_PATH"
-        value = "./application-code/ecsdemo-nodejs/."
-      },
-    ]
-  }
-
-  create_iam_role = true
-  iam_role_name   = "${module.ecs_service_definition.name}-codebuild"
-  ecr_repository  = module.container_image_ecr.repository_arn
-
-  tags = local.tags
-}
-
-module "codepipeline_ci_cd" {
-  source = "../../modules/codepipeline"
-
-  name         = "pipeline-${module.ecs_service_definition.name}"
-  service_role = module.codepipeline_ci_cd.codepipeline_role_arn
-  s3_bucket    = module.codepipeline_s3_bucket
-  sns_topic    = aws_sns_topic.codestar_notification.arn
-
-  stage = [{
-    name = "Source"
-    action = [{
-      name             = "Source"
-      category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
-      version          = "1"
-      input_artifacts  = []
-      output_artifacts = ["SourceArtifact"]
-      configuration = {
-        OAuthToken           = data.aws_secretsmanager_secret_version.github_token.secret_string
-        Owner                = var.repository_owner
-        Repo                 = var.repository_name
-        Branch               = var.repository_branch
-        PollForSourceChanges = true
-      }
-    }],
-    }, {
-    name = "Build"
-    action = [{
-      name             = "Build_app"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      version          = "1"
-      input_artifacts  = ["SourceArtifact"]
-      output_artifacts = ["BuildArtifact_app"]
-      configuration = {
-        ProjectName = module.codebuild_ci.project_id
-      }
-    }],
-    }, {
-    name = "Deploy"
-    action = [{
-      name            = "Deploy_app"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "ECS"
-      version         = "1"
-      input_artifacts = ["BuildArtifact_app"]
-      configuration = {
-        ClusterName = data.aws_ecs_cluster.core_infra.cluster_name
-        ServiceName = module.ecs_service_definition.name
-        FileName    = "imagedefinition.json"
-      }
-    }],
-  }]
-
-  create_iam_role = true
-  iam_role_name   = "${module.ecs_service_definition.name}-pipeline"
 
   tags = local.tags
 }
--- examples/lb-service/main.tf
+++ examples/lb-service/main.tf
@@ -9,7 +9,7 @@ locals {
   region = "us-west-2"
 
   container_port = 3000 # Container port is specific to this app example
-  container_name = "nodejs-frontend"
+  container_name = "ecsdemo-frontend"
 
   tags = {
     Blueprint  = local.name
@@ -20,20 +20,6 @@ locals {
 ################################################################################
 # ECS Blueprint
 ################################################################################
-
-module "container_image_ecr" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "~> 1.4"
-
-  repository_name = local.container_name
-
-  repository_force_delete           = true
-  create_lifecycle_policy           = false
-  repository_read_access_arns       = [one(data.aws_iam_roles.ecs_core_infra_exec_role.arns)]
-  repository_read_write_access_arns = [module.codepipeline_ci_cd.codepipeline_role_arn]
-
-  tags = local.tags
-}
 
 module "service_alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -108,7 +94,7 @@ resource "aws_service_discovery_service" "this" {
 }
 
 module "ecs_service_definition" {
-  source = "github.com/nvpnathan/terraform-aws-ecs//modules/service"
+  source = "github.com/clowdhaus/terraform-aws-ecs//modules/service"
 
   name          = local.name
   desired_count = 3
@@ -155,7 +141,7 @@ module "ecs_service_definition" {
   container_definitions = {
     main_container = {
       name                     = local.container_name
-      image                    = module.container_image_ecr.repository_url
+      image                    = "public.ecr.aws/aws-containers/ecsdemo-frontend"
       readonly_root_filesystem = false
 
       port_mappings = [{
@@ -166,156 +152,7 @@ module "ecs_service_definition" {
     }
   }
 
-  ignore_task_definition_changes = true
-
-  tags = local.tags
-}
-
-################################################################################
-# CodePipeline and CodeBuild for CI/CD
-################################################################################
-
-module "codepipeline_s3_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 3.0"
-
-  bucket = "codepipeline-${local.region}-${random_id.this.hex}"
-  acl    = "private"
-
-  # For example only - please re-evaluate for your environment
-  force_destroy = true
-
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = local.tags
-}
-
-resource "aws_sns_topic" "codestar_notification" {
-  name = local.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid      = "WriteAccess"
-        Effect   = "Allow"
-        Action   = "sns:Publish"
-        Resource = "arn:aws:sns:${local.region}:${data.aws_caller_identity.current.account_id}:${local.name}"
-        Principal = {
-          Service = "codestar-notifications.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = local.tags
-}
-
-module "codebuild_ci" {
-  source = "../../modules/codebuild"
-
-  name           = "codebuild-${module.ecs_service_definition.name}"
-  service_role   = module.codebuild_ci.codebuild_role_arn
-  buildspec_path = "./application-code/nodejs-demoapp/templates/buildspec-frontend.yml"
-  s3_bucket      = module.codepipeline_s3_bucket
-
-  environment = {
-    privileged_mode = true
-    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-    environment_variables = [
-      {
-        name  = "REPO_URL"
-        value = module.container_image_ecr.repository_url
-        }, {
-        name  = "CONTAINER_NAME"
-        value = local.container_name
-        }, {
-        name  = "FOLDER_PATH"
-        value = "./application-code/nodejs-demoapp/."
-      }
-    ]
-  }
-
-  create_iam_role = true
-  iam_role_name   = "${module.ecs_service_definition.name}-codebuild-${random_id.this.hex}"
-  ecr_repository  = module.container_image_ecr.repository_arn
-
-  tags = local.tags
-}
-
-module "codepipeline_ci_cd" {
-  source = "../../modules/codepipeline"
-
-  name         = "pipeline-${module.ecs_service_definition.name}"
-  service_role = module.codepipeline_ci_cd.codepipeline_role_arn
-  s3_bucket    = module.codepipeline_s3_bucket
-  sns_topic    = aws_sns_topic.codestar_notification.arn
-
-  stage = [{
-    name = "Source"
-    action = [{
-      name             = "Source"
-      category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
-      version          = "1"
-      input_artifacts  = []
-      output_artifacts = ["SourceArtifact"]
-      configuration = {
-        OAuthToken           = data.aws_secretsmanager_secret_version.github_token.secret_string
-        Owner                = var.repository_owner
-        Repo                 = var.repository_name
-        Branch               = var.repository_branch
-        PollForSourceChanges = true
-      }
-    }],
-    }, {
-    name = "Build"
-    action = [{
-      name             = "Build_app"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      version          = "1"
-      input_artifacts  = ["SourceArtifact"]
-      output_artifacts = ["BuildArtifact_app"]
-      configuration = {
-        ProjectName = module.codebuild_ci.project_id
-      }
-    }],
-    }, {
-    name = "Deploy"
-    action = [{
-      name            = "Deploy_app"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "ECS"
-      version         = "1"
-      input_artifacts = ["BuildArtifact_app"]
-      configuration = {
-        ClusterName = data.aws_ecs_cluster.core_infra.cluster_name
-        ServiceName = module.ecs_service_definition.name
-        FileName    = "imagedefinition.json"
-      }
-    }],
-  }]
-
-  create_iam_role = true
-  iam_role_name   = "${module.ecs_service_definition.name}-pipeline-${random_id.this.hex}"
+  ignore_task_definition_changes = false
 
   tags = local.tags
 }
@@ -323,10 +160,6 @@ module "codepipeline_ci_cd" {
 ################################################################################
 # Supporting Resources
 ################################################################################
-
-resource "random_id" "this" {
-  byte_length = "2"
-}
 
 data "aws_secretsmanager_secret" "github_token" {
   name = var.github_token_secret_name
