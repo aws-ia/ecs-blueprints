df532b5481a7f52f315e42fcc38390fb5e67e21e
jooyoung <joozero@amazon.com>
2023-04-04 13:49:40 -0700

    remove unnecessary codes

--- cdk/examples/components/core_infrastructure_construct.py
+++ cdk/examples/components/core_infrastructure_construct.py
@@ -119,6 +119,3 @@ class CoreInfrastructureConstruct(Construct):
         self.ecs_cluster_id = self.ecs_cluster.cluster_arn
         self.ecs_task_execution_role_arn = self.ecs_task_execution_role.role_arn
         self.ecs_task_execution_role_name = self.ecs_task_execution_role.role_name
-        # self.ecs_cluster_security_groups = [
-        #     sg for sg in self.ecs_cluster.connections.security_groups
-        # ]
