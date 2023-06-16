c77a5f7ded0d67b24d1e180b42f9d04d19a97ff1
Jooyoung Kim <59524380+joozero@users.noreply.github.com>
2023-06-16 10:25:30 -0700

    AWS FIS for ECS template change (#150)
    * change the configuration
    
    * add fine grained iam policy

--- cdk/examples/fis_service/lib/fis_service_stack.py
+++ cdk/examples/fis_service/lib/fis_service_stack.py
@@ -98,6 +98,7 @@ class FISServiceStack(Stack):
             environment={
                 "MANAGED_INSTANCE_ROLE_NAME": self.ssm_managed_instance_role.role_name
             },
+            essential=False,
             memory_reservation_mib=64,
             command=[
                 "/bin/bash",
@@ -105,7 +106,7 @@ class FISServiceStack(Stack):
                 "set -e; yum upgrade -y; yum install jq procps awscli -y; term_handler() { echo \"Deleting SSM activation $ACTIVATION_ID\"; if ! aws ssm delete-activation --activation-id $ACTIVATION_ID --region $ECS_TASK_REGION; then echo \"SSM activation $ACTIVATION_ID failed to be deleted\" 1>&2; fi; MANAGED_INSTANCE_ID=$(jq -e -r .ManagedInstanceID /var/lib/amazon/ssm/registration); echo \"Deregistering SSM Managed Instance $MANAGED_INSTANCE_ID\"; if ! aws ssm deregister-managed-instance --instance-id $MANAGED_INSTANCE_ID --region $ECS_TASK_REGION; then echo \"SSM Managed Instance $MANAGED_INSTANCE_ID failed to be deregistered\" 1>&2; fi; kill -SIGTERM $SSM_AGENT_PID; }; trap term_handler SIGTERM SIGINT; if [[ -z $MANAGED_INSTANCE_ROLE_NAME ]]; then echo \"Environment variable MANAGED_INSTANCE_ROLE_NAME not set, exiting\" 1>&2; exit 1; fi; if ! ps ax | grep amazon-ssm-agent | grep -v grep > /dev/null; then if [[ -n $ECS_CONTAINER_METADATA_URI_V4 ]] ; then echo \"Found ECS Container Metadata, running activation with metadata\"; TASK_METADATA=$(curl \"${ECS_CONTAINER_METADATA_URI_V4}/task\"); ECS_TASK_AVAILABILITY_ZONE=$(echo $TASK_METADATA | jq -e -r '.AvailabilityZone'); ECS_TASK_ARN=$(echo $TASK_METADATA | jq -e -r '.TaskARN'); ECS_TASK_REGION=$(echo $ECS_TASK_AVAILABILITY_ZONE | sed 's/.$//'); ECS_TASK_AVAILABILITY_ZONE_REGEX='^(af|ap|ca|cn|eu|me|sa|us|us-gov)-(central|north|(north(east|west))|south|south(east|west)|east|west)-[0-9]{1}[a-z]{1}$'; if ! [[ $ECS_TASK_AVAILABILITY_ZONE =~ $ECS_TASK_AVAILABILITY_ZONE_REGEX ]]; then echo \"Error extracting Availability Zone from ECS Container Metadata, exiting\" 1>&2; exit 1; fi; ECS_TASK_ARN_REGEX='^arn:(aws|aws-cn|aws-us-gov):ecs:[a-z0-9-]+:[0-9]{12}:task/[a-zA-Z0-9_-]+/[a-zA-Z0-9]+$'; if ! [[ $ECS_TASK_ARN =~ $ECS_TASK_ARN_REGEX ]]; then echo \"Error extracting Task ARN from ECS Container Metadata, exiting\" 1>&2; exit 1; fi; CREATE_ACTIVATION_OUTPUT=$(aws ssm create-activation --iam-role $MANAGED_INSTANCE_ROLE_NAME --tags Key=ECS_TASK_AVAILABILITY_ZONE,Value=$ECS_TASK_AVAILABILITY_ZONE Key=ECS_TASK_ARN,Value=$ECS_TASK_ARN --region $ECS_TASK_REGION); ACTIVATION_CODE=$(echo $CREATE_ACTIVATION_OUTPUT | jq -e -r .ActivationCode); ACTIVATION_ID=$(echo $CREATE_ACTIVATION_OUTPUT | jq -e -r .ActivationId); if ! amazon-ssm-agent -register -code $ACTIVATION_CODE -id $ACTIVATION_ID -region $ECS_TASK_REGION; then echo \"Failed to register with AWS Systems Manager (SSM), exiting\" 1>&2; exit 1; fi; amazon-ssm-agent & SSM_AGENT_PID=$!; wait $SSM_AGENT_PID; else echo \"ECS Container Metadata not found, exiting\" 1>&2; exit 1; fi; else echo \"SSM agent is already running, exiting\" 1>&2; exit 1; fi"
             ]
         )
-
+        
     @property
     def vpc(self):
         if not self._vpc:
@@ -158,18 +159,21 @@ class FISServiceStack(Stack):
             "ECSTaskRole",
             assumed_by=ServicePrincipal("ecs-tasks"),
         )
-
-        inline_policy = PolicyStatement(
+        self.ecs_task_role.add_to_policy(PolicyStatement(
             effect=Effect.ALLOW,
             actions=[
                 "iam:PassRole",
+            ],
+            resources=['arn:aws:iam::'+self.stack_props.account_number+':role/*'],
+        ))
+        self.ecs_task_role.add_to_policy(PolicyStatement(
+            effect=Effect.ALLOW,
+            actions=[
                 "ssm:CreateActivation",
                 "ssm:AddTagsToResource",
             ],
             resources=["*"],
-        )
-
-        self.ecs_task_role.add_to_policy(inline_policy)
+        ))
 
     def _create_ssm_managed_instance_role(self):
         self.ssm_managed_instance_role = Role(
@@ -182,15 +186,23 @@ class FISServiceStack(Stack):
                 )
             ]
         )
-        inline_policy = PolicyStatement(
-            effect=Effect.ALLOW,
-            actions=[
-                "ssm:DeleteActivation",
-                "ssm:DeregisterManagedInstance",
-            ],
-            resources=["*"],
-        )
-        self.ssm_managed_instance_role.add_to_policy(inline_policy)
+        self.ssm_managed_instance_role.add_to_policy(
+            PolicyStatement(
+                effect=Effect.ALLOW,
+                actions=[
+                    "ssm:DeleteActivation",
+                ],
+                resources=["*"],
+        ))
+        self.ssm_managed_instance_role.add_to_policy(
+            PolicyStatement(
+                effect=Effect.ALLOW,
+                actions=[
+                    "ssm:DeregisterManagedInstance",
+                ],
+                resources=['arn:aws:ssm:'+self.stack_props.aws_region+':'+self.stack_props.account_number+':managed-instance/*'],
+        ))
+
 
     def validate_stack_props(self):
         if (
