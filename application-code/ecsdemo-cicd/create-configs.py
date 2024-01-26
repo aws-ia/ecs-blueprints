8f33dd46bb3683c1f859dc669da177e791be3fda
Hans Nesbitt <hansnesb@amazon.com>
2024-01-25 20:06:43 -0800

    added copywrite to create-configs.

--- application-code/ecsdemo-cicd/create-configs.py
+++ application-code/ecsdemo-cicd/create-configs.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. SPDX-License-Identifier: MIT-0
+
 #Example Command
 # python3 create-configs.py public.ecr.aws/docker/library/httpd:latest core-infra ecsdemo-frontend development us-west-2
 
