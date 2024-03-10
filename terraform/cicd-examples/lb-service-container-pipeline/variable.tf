ae808b016d929b3a6f4c83592c440d6ddb260cb9
jooyoung <joozero@amazon.com>
2024-03-09 21:45:02 -0800

    change lint

--- terraform/cicd-examples/lb-service-container-pipeline/variable.tf
+++ terraform/cicd-examples/lb-service-container-pipeline/variable.tf
@@ -3,9 +3,3 @@ variable "region" {
   default     = "us-west-2"
   description = "AWS region you want to deploy to."
 }
-
-variable "s3_bucket" {
-  type        = string
-  default     = "terraform-20240310021637223800000001"
-  description = "s3 bucket"
-}
