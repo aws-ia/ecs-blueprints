69800b7750ee475168fda74ac9bc69446e387532
jooyoung <joozero@amazon.com>
2024-09-23 11:18:33 -0700

    add rag sample

---
+++ cdk/examples/generative_ai_rag/lib/gen_ai_rag_stack.py
@@ -0,0 +1,169 @@
+from aws_cdk import PhysicalName, Stack
+from aws_cdk.aws_ec2 import Vpc
+from aws_cdk.aws_ecs import Cluster, ContainerImage, LogDriver
+from aws_cdk.aws_ecr_assets import Platform
+from aws_cdk.aws_ecs_patterns import (
+    ApplicationLoadBalancedFargateService,
+    ApplicationLoadBalancedTaskImageOptions,
+)
+from aws_cdk.aws_iam import (
+    Role,
+    PolicyStatement,
+    Effect,
+    ServicePrincipal
+)
+from aws_cdk.aws_logs import LogGroup, RetentionDays
+from aws_cdk.aws_servicediscovery import PrivateDnsNamespace
+from generative_ai_rag.lib.gen_ai_rag_stack_props import GenAIRagServiceStackProps
+
+class GenAIRagServiceStack(Stack):
+    def __init__(
+        self,
+        scope: Stack,
+        id: str,
+        gen_ai_service_stack_prop: GenAIRagServiceStackProps,
+        **kwargs
+    ):
+
+        super().__init__(scope, id, **kwargs)
+
+        self.stack_props = gen_ai_service_stack_prop
+        self._ecs_cluster = None
+        self._ecs_task_execution_role = None
+        self._vpc = self.stack_props.vpc if self.stack_props.vpc else None
+        self._sd_namespace = (
+            self.stack_props.sd_namespace if self.stack_props.sd_namespace else None
+        )
+
+        # Amazon CloudWatch log group
+        log_group = LogGroup(
+            self,
+            "GenAIServiceLogGroup",
+            retention=RetentionDays.ONE_WEEK,
+            log_group_name=PhysicalName.GENERATE_IF_NEEDED,
+        )
+
+        # ECS Task Role
+        self.task_role = Role(
+            self,
+            'GenAIServiceTaskRole',
+            role_name='GenAIServiceTaskRole',
+            assumed_by=ServicePrincipal('ecs-tasks.amazonaws.com'),
+            description='ECS Task role for Gen AI service'
+        )
+
+        # AWS Fargate task container defintion
+        fargate_task_image = ApplicationLoadBalancedTaskImageOptions(
+            container_name=self.stack_props.container_name,
+            # build container image from local folder
+            # image=ContainerImage.from_asset("web-app", platform=Platform.LINUX_AMD64),
+            # load pre-built image from public repository
+            image=ContainerImage.from_asset("web-app", platform=Platform.LINUX_AMD64),
+            environment={'region': self.stack_props.aws_region},
+            container_port=self.stack_props.container_port,
+            execution_role=self.ecs_task_execution_role,
+            log_driver=LogDriver.aws_logs(
+                stream_prefix="ecs",
+                log_group=log_group,
+            ),
+            task_role=self.task_role
+        )
+
+        # ECS service with Application Load Balancer
+        self.fargate_service = ApplicationLoadBalancedFargateService(
+            self,
+            "GenAIRAGFargateLBService",
+            service_name=self.stack_props.service_name,
+            cluster=self.ecs_cluster,
+            cpu=int(self.stack_props.task_cpu),
+            memory_limit_mib=int(self.stack_props.task_memory),
+            desired_count=self.stack_props.desired_count,
+            enable_execute_command=True,
+            public_load_balancer=True,
+            task_image_options=fargate_task_image,
+            enable_ecs_managed_tags=True
+        )
+
+        # Add ECS Task IAM Role
+        self.task_role.add_to_policy(
+            PolicyStatement(
+                effect=Effect.ALLOW,
+                actions = ["ssm:GetParameter"],
+                resources = ["arn:aws:ssm:*"]
+            )
+        )
+
+        self.task_role.add_to_policy(
+            PolicyStatement(
+                effect=Effect.ALLOW,
+                actions=["bedrock:InvokeAgent", "bedrock:InvokeModel"],
+                resources=["*"]
+            )
+        )
+
+        # ECS Service Auto Scaling policy
+        scalable_target = self.fargate_service.service.auto_scale_task_count(
+            min_capacity=1, max_capacity=5
+        )
+
+        scalable_target.scale_on_cpu_utilization(
+            "CpuScaling", target_utilization_percent=50
+        )
+
+    @property
+    def vpc(self):
+        if not self._vpc:
+            self._vpc = Vpc.from_lookup(
+                self, "VpcLookup", vpc_name=self.stack_props.vpc_name
+            )
+
+        return self._vpc
+
+    @property
+    def sd_namespace(self):
+        if not self._sd_namespace:
+            self._sd_namespace = (
+                PrivateDnsNamespace.from_private_dns_namespace_attributes(
+                    self,
+                    "SDNamespaceLookup",
+                    namespace_name=self.stack_props.namespace_name,
+                    namespace_arn=self.stack_props.namespace_arn,
+                    namespace_id=self.stack_props.namespace_id,
+                )
+            )
+
+        return self._sd_namespace
+
+    @property
+    def ecs_cluster(self):
+        if not self._ecs_cluster:
+            self._ecs_cluster = Cluster.from_cluster_attributes(
+                self,
+                "EcsClusterLookup",
+                cluster_name=self.stack_props.ecs_cluster_name,
+                security_groups=[],
+                vpc=self.vpc,
+                default_cloud_map_namespace=self.sd_namespace,
+            )
+
+        return self._ecs_cluster
+
+    @property
+    def ecs_task_execution_role(self) -> Role:
+        if not self._ecs_task_execution_role:
+            self._ecs_task_execution_role = Role.from_role_arn(
+                self,
+                "EcsTaskRoleFromArn",
+                self.stack_props.ecs_task_execution_role_arn,
+            )
+
+        return self._ecs_task_execution_role
+
+    def validate_stack_props(self):
+        if (
+            self.stack_props.account_number == "<ACCOUNT_NUMBER>"
+            or self.stack_props.aws_region == "<REGION>"
+        ):
+            raise ValueError(
+                "Environment values needs to be set for account_number, aws_region"
+            )
---
+++ cdk/examples/generative_ai_rag/web-app/pages/foundation_model.py
@@ -0,0 +1,84 @@
+import boto3.exceptions
+import streamlit as st
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+st.caption("Using Calude 3 Haiku from Anthropic")
+
+@st.cache_data
+def get_parameter(name):
+    try:
+        ssm = boto3.client('ssm')
+        param = ssm.get_parameter(Name=name,WithDecryption=True)
+        return param['Parameter']['Value']
+    except:
+        return
+
+st.title("💬 Chat")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "Ask me something about reinvent 2024?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": prompt
+        }
+    )
+
+    # Use the native inference API to send a text message to Anthropic Claude.
+
+    # Create a Bedrock Runtime client in the AWS Region of your choice.
+    client = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+    # Set the model ID, e.g., Claude 3 Haiku.
+    model_id = "anthropic.claude-3-haiku-20240307-v1:0"
+
+    # Format the request payload using the model's native structure.
+    native_request = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 512,
+        "temperature": 0.5,
+        "messages": [
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": prompt}],
+            }
+        ],
+    }
+
+    # Convert the native request to JSON.
+    request = json.dumps(native_request)
+
+    try:
+        # Invoke the model with the request.
+        response = client.invoke_model(modelId=model_id, body=request)
+
+    except (ClientError, Exception) as e:
+        msg = f"ERROR: Can't invoke '{model_id}'. Reason: {e}"
+        st.session_state.messages.append(
+            {
+                "role": "assistant", 
+                "content": msg
+            }
+        )
+        st.chat_message("assistant").write(msg)
+
+    # Decode the response body.
+    model_response = json.loads(response["body"].read())
+
+    # Extract and print the response text.
+    msg = model_response["content"][0]["text"]
+    st.session_state.messages.append(
+        {
+            "role": "assistant", 
+            "content": msg
+        }
+    )
+    st.chat_message("assistant").write(msg)
