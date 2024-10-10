e0e8c7b66910dcd5007abe7c4f3f5f20f33d43a5
Shin, Jungseob <jungseob@amazon.com>
2024-10-10 17:51:32 +0900

    Remove agent feature
    - remove action-group.yaml
    - remove agent stack
    - update reinvent.txt
    - update web-app

--- cdk/examples/other_stack/bedrock_agent_stack.py
+++ cdk/examples/other_stack/bedrock_agent_stack.py
@@ -1,21 +1,13 @@
-from aws_cdk import Duration, Stack, RemovalPolicy
-from aws_cdk.aws_iam import (
-    ManagedPolicy,
-    Role,
-    ServicePrincipal,
-    PolicyStatement,
-    Effect
-)
+from aws_cdk import Stack, RemovalPolicy
 from aws_cdk.aws_ssm import StringParameter
 import aws_cdk.aws_dynamodb as dynamodb
-import aws_cdk.aws_lambda as lambda_
-import aws_cdk.aws_lambda_python_alpha as lambda_python
 import aws_cdk.aws_s3 as s3
 import os
 import aws_cdk.aws_s3_deployment as s3_deployment
 from cdklabs.generative_ai_cdk_constructs import (
     bedrock
 )
+
 from constructs import Construct
 
 class BedrockAgentStack(Stack):
@@ -58,89 +50,21 @@ class BedrockAgentStack(Stack):
             name="bedrock-knowledge-base-for-reinvent",
             description="Amazon Bedrock Knowledge Base for re:Invent 2024",
             embeddings_model= bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V2_1024,
-            instruction= 'Use this knowledge base to answer questions about re:Invent 2024. It contains information of sessions.'
+            instruction= 'Use this knowledge base to answer questions about re:Invent 2024 session information. It contains title, session code, sponsor, description, session type, topic, areas of interest, level, target roles, venue, date and time, prerequisites, key points, and related AWS services.'
         )
 
         # create bedrock knowledge base data source
         self.knowledge_base_data_source = bedrock.S3DataSource(self, 'KnowledgeBaseDataSource',
-            bucket= self.bucket,
-            knowledge_base= self.knowledge_base,
-            data_source_name= 'ReinventAgendaItem',
-            chunking_strategy= bedrock.ChunkingStrategy.HIERARCHICAL_TITAN
+            bucket=self.bucket,
+            knowledge_base=self.knowledge_base,
+            data_source_name='ReinventSessionInformationText',
+            chunking_strategy= bedrock.ChunkingStrategy.FIXED_SIZE,
         )
-
-        # create bedrock agent
-        self.agent = bedrock.Agent(
-            self,
-            "BedrockAgent",
-            name="bedrock-agent-for-reinvent",
-            description="Amazon bedrock Agent for re:Invent 2024",
-            foundation_model=bedrock.BedrockFoundationModel.ANTHROPIC_CLAUDE_HAIKU_V1_0,
-            instruction="You are a helpful and friendly agent that answers questions about re:Invent 2024",
-        )
-
-        # integrate knowledge base to bedrock agent
-        self.agent.add_knowledge_base(self.knowledge_base)
-
-        self.agent_alias = self.agent.add_alias(alias_name="bedrock-agent-for-reinvent")
-
-        # create bedrock agent action group function
-        action_group_lambda_role = Role(
-            self,
-            "LambdaExecutionRole",
-            assumed_by=ServicePrincipal("lambda.amazonaws.com"),
-            managed_policies=[
-                ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
-            ]
-        )
-
-        action_group_lambda_role.add_to_policy(PolicyStatement(
-            effect=Effect.ALLOW,
-            actions=[
-                "dynamodb:PutItem",
-                "dynamodb:GetItem",
-                "dynamodb:Scan"
-            ],
-            resources=[self.bookmark_table.table_arn]
-        ))
-
-        self.agent_action_group_function = lambda_python.PythonFunction(
-            self,
-            "BookmarkLambdaFunction",
-            runtime=lambda_.Runtime.PYTHON_3_10,
-            entry="lambda",
-            index="index.py",
-            handler="lambda_handler",
-            timeout=Duration.seconds(300),
-            role=action_group_lambda_role,
-            environment={
-                "TABLE_NAME": self.bookmark_table.table_name
-            }
-        )
-
-
-        # create bedrock agent action group
-        self.actionGroup = bedrock.AgentActionGroup(self,
-            "ReinventBookmarkActionGroup",
-            action_group_name="reinvent-bookmark",
-            description="Use these functions to get information about re:Invent 2024 sessions.",
-            action_group_executor= bedrock.ActionGroupExecutor(
-            lambda_=self.agent_action_group_function
-            ),
-            action_group_state="ENABLED",
-            api_schema=bedrock.ApiSchema.from_asset("assets/action-group.yaml"))
-        self.agent.add_action_group(self.actionGroup)
 
         # create parameter store
         self.agent_id_parameter = StringParameter(
             self,
-            "BedrockAgentIdParameter",
-            parameter_name="agent_id",
-            string_value=self.agent.agent_id
-        )
-        self.agent_alias_id_parameter = StringParameter(
-            self,
-            "BedrockAgentAliasIdParameter",
-            parameter_name="agent_alias_id",
-            string_value=self.agent_alias.alias_id
+            "BedrockKnowledgeBaseIdParameter",
+            parameter_name="knowledge_base_id",
+            string_value=self.knowledge_base.knowledge_base_id
         )
