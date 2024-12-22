c905a30a1ab121b241659ab5325cf100aa67e184
Maicon Alegre <alegrm@amazon.com>
2024-12-23 09:00:45 +1030

    update AMI to Amazon Linux 2023

--- terraform/ec2-examples/core-infra-ipv6/main.tf
+++ terraform/ec2-examples/core-infra-ipv6/main.tf
@@ -114,7 +114,7 @@ module "vpc" {
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html#ecs-optimized-ami-linux
 data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended"
 }
 
 module "autoscaling" {
