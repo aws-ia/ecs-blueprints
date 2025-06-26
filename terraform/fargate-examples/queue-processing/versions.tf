e1cba8f51f037a439ec387805bb55c493280e0d7
sfloresk <sfkanter@amazon.com>
2025-06-26 15:37:03 -0600

    Fix random version

--- terraform/fargate-examples/queue-processing/versions.tf
+++ terraform/fargate-examples/queue-processing/versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "= 5.100.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "= 5.100.0"
+      version = ">= 3.0"
     }
   }
 }
